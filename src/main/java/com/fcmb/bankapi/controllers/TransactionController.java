dc34b1216335ffda5830b317a3bf896739cf3eeb
chinemeze847 <thankgodchinemeze@gmail.com>
2023-09-01 20:47:41 +0100

    completed the get transactions controller

--- src/main/java/com/fcmb/bankapi/controllers/TransactionController.java
+++ src/main/java/com/fcmb/bankapi/controllers/TransactionController.java
@@ -1,9 +1,29 @@
 package com.fcmb.bankapi.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.fcmb.bankapi.dtos.TransferDto;
+import com.fcmb.bankapi.models.Transaction;
+import com.fcmb.bankapi.services.TransactionService;
 
 @RestController
 @RequestMapping("/transactions")
 public class TransactionController {
+
+	@Autowired
+	private TransactionService transactionService;
+
+	public TransactionController(TransactionService transactionService) {
+		this.transactionService = transactionService;
+	}
+
+	@GetMapping("/{account_num}")
+	public ResponseEntity<List<Transaction>> getTransactions(@PathVariable String account_num ){
+		List<Transaction> transactions = transactionService.findByAccountNumber(account_num);
+		return ResponseEntity.ok(transactions);
+	}
 }
